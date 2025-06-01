9f4b3187f0225216655c040fb838272483c4c334
alexVanDerBrugge <143549465+alexVanDerBrugge@users.noreply.github.com>
2025-05-31 22:52:12 -0400

    Add tile flips

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -9,11 +9,13 @@ public class GridManager : MonoBehaviour
     public int size = 5;
     private float hexSize = 0.8f;
     public GameObject player;
+    private Dictionary<string, GameObject> hexes = new Dictionary<string, GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
         GenerateHexagonalGrid(size);
+        Reveal(0, 0, 1);
     }
 
     void Update()
@@ -28,6 +30,24 @@ public class GridManager : MonoBehaviour
                 GameObject clickedHex = hit.collider.gameObject;
                 HexPos hex = clickedHex.GetComponent<HexPos>();
                 player.GetComponent<PlayerMovement>().MoveToPos(hex.q, hex.r);
+                Reveal(hex.q, hex.r, 1);
+            }
+        }
+    }
+
+    void Reveal(int centerQ, int centerR, int radius)
+    {
+        // Rotate the hexagons 180 degrees around the x axis
+        for (int q = -radius; q <= radius; q++)
+        {
+            int r1 = Mathf.Max(-radius, -q - radius);
+            int r2 = Mathf.Min(radius, -q + radius);
+            for (int r = r1; r <= r2; r++)
+            {
+                if (hexes.TryGetValue($"{centerQ + q},{centerR + r}", out GameObject hexObj))
+                {
+                    hexObj.transform.rotation = Quaternion.Euler(180, 90, 0);
+                }
             }
         }
     }
@@ -42,6 +62,7 @@ public class GridManager : MonoBehaviour
             {
                 // rotate 90 degrees around the Y-axis
                 GameObject hexObj = Instantiate(hexagon, transform.position, Quaternion.Euler(0, 90, 0));
+                hexes[$"{q},{r}"] = hexObj;
                 hexObj.transform.localScale = new Vector3(hexSize, hexSize, hexSize);
                 HexPos pos = hexObj.AddComponent<HexPos>();
                 pos.q = q;
